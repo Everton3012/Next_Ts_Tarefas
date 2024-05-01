a1d34dd218d9078ced78e01d971365ea9dd316b8
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-05-01 15:42:07 -0300

    barra de navegação finalizada

---
+++ src/components/header/index.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+export function Header() {
+    return (
+        <header className={styles["header"]}>
+            <section className={styles["content"]}>
+                <nav className={styles["nav"]}>
+                    <Link href={"/"}>
+                        <h1 className={styles["logo"]}>Tareafas<span>+</span></h1>
+                    </Link>
+                    <Link className={styles["link"]} href={"/dashboard"}>
+                        Meu Paienl
+                    </Link>
+                </nav>
+                <button className={styles["loginButton"]}>Acessar</button>
+            </section>
+        </header>
+    )
+}
