92d5d4b2eb8f5c4b3f515de1a5e99686003ac1fd
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-05-01 23:23:06 -0300

    sistema de login finalizado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,12 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 import Link from "next/link";
 
 import styles from "./styles.module.css";
 
 export function Header() {
+
+    const { data: session, status } = useSession();
+
     return (
         <header className={styles["header"]}>
             <section className={styles["content"]}>
@@ -10,11 +14,19 @@ export function Header() {
                     <Link href={"/"}>
                         <h1 className={styles["logo"]}>Tareafas<span>+</span></h1>
                     </Link>
-                    <Link className={styles["link"]} href={"/dashboard"}>
-                        Meu Painel
-                    </Link>
+                    {session?.user && (
+                        <Link className={styles["link"]} href={"/dashboard"}>
+                            Meu Painel
+                        </Link>
+                    )}
                 </nav>
-                <button className={styles["loginButton"]}>Acessar</button>
+                {status === "loading" ? (
+                    <></>
+                ) : session ? (
+                    <button className={styles["loginButton"]} onClick={() => signOut()}>Olá {session?.user?.name}</button>
+                ) : (
+                    <button className={styles["loginButton"]} onClick={() => signIn("google")} >Acessar</button>
+                )}
             </section>
         </header>
     )
