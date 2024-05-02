61b7e395611f9c0c0ee95e310e314f6fd6b1ad21
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-05-01 23:32:13 -0300

    rota dashboard protegida

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from "next";
 import styles from "./style.module.css";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
@@ -12,3 +14,20 @@ export default function Dashboard() {
     </section>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  //console.log(session);
+
+  if(!session?.user){
+    return{
+      redirect:{
+        destination:"/",
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props: {},
+  };
+};
