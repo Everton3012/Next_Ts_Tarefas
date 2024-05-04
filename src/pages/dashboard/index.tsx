2eba8ec04a7da01835f1bc03dcf182c3cd142e46
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-05-03 23:48:53 -0300

    css de artigos pronto

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 
 import { getSession } from "next-auth/react";
 import Textarea from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -26,6 +28,37 @@ export default function Dashboard() {
               </button>
             </form>
           </div>
+        </section>
+        <section className={styles["taskContainer"]}>
+          <h1>Minhas tarefas</h1>
+          <article className={styles["task"]}>
+            <div className={styles["tagContainer"]}>
+              <label className={styles["tag"]}>PUBLICO</label>
+              <button className={styles["shareButton"]}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles["taskContent"]}>
+              <p>Minha primeira tarefa</p>
+              <button className={styles["trashButton"]}>
+                <FaTrash size={24} color="ea3140"/>
+              </button>
+            </div>
+          </article>
+          <article className={styles["task"]}>
+            <div className={styles["tagContainer"]}>
+              <label className={styles["tag"]}>PUBLICO</label>
+              <button className={styles["shareButton"]}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles["taskContent"]}>
+              <p>Minha primeira tarefa</p>
+              <button className={styles["trashButton"]}>
+                <FaTrash size={24} color="ea3140"/>
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </section>
