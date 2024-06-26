fe14f37bedbc9c732c6343e41cbfe6d815828eeb
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-25 21:15:28 -0300

    analitics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components/header";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
